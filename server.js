acbed38371250a5d6548946c039fdb932a7c2b86
TIM BIN <tim.bin@pnc.lan>
2022-05-12 10:10:23 +0700

    fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 
 require('dotenv').config()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 443
 app.use(express.urlencoded({ extended : true }))
 app.use(express.json())
 
